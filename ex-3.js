790b7d4fc7b06831e3a75c16b2f38cd20076e96a
Cynthy YD <cynthyyd@Cynthys-MacBook-Pro.local>
2023-10-07 03:18:18 +0700

    add: exercise answers 1-6

--- ex-3.js
+++ ex-3.js
@@ -1,2 +1,19 @@
 // Exercise #3 Declaring Arrow Function
 // Start coding here
+let add = (a, b) => {
+    return a + b;
+};
+let subtract = (a, b) => {
+    return a - b;
+};
+let multiply = (a, b) => {
+    return a * b;
+};
+let divide = (a, b) => {
+    return a / b;
+};
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,10 @@
 // Exercise #5 Min Function
 // Start coding here
+
+function findMinNumber(a, b) {
+    return Math.min(a, b);
+}
+
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
